41dd4b258ef8319d50e41119cd21289bcabfdd64
jul10l1r4 <jul10l1r4@ufrn.edu.br>
2018-11-24 17:46:29 -0300

    Performance teste

--- branchmark.sh
+++ branchmark.sh
@@ -4,13 +4,17 @@
 buffer="";
 
 # sendo claro
-echo "Teste de performance entre ECHO, PRINTF e CAT"
-echo "Armazendo variáveis"
+echo """
+Teste de performance entre ECHO, PRINTF e CAT
+
+##################################################
+Ele mostrará uma série de caracteres em apenas
+uma chamada"""
 
 # fazendo o processador tremer
 for i in $(seq 10000)
 do
-		buffer+="@#$%%&*I&FDFSRWEGsy€®³®ŧŋđðđðŋæ"
+		buffer+='@#$%%&*I&FDFSRWEGsy€®³®ŧŋđðđðŋæ'
 done;
 
 # realizando testes
@@ -24,7 +28,7 @@ function priTest (){
 }
 
 function printfTest () {
-	printf "$buffer" > "/dev/stdout" > /dev/null
+	printf "$buffer"	> /dev/stdout 2&>-# /dev/null
 }
 
 function catTest (){
@@ -35,14 +39,62 @@ function catTest (){
 
 # Saída do resultado
 
-echo -e "\n### ECHO ###"
+echo -e "--------------------------------------------------\n### ECHO ###"
 time echoTest 
 
-echo -e "\n### PRINTF ###"
+echo -e "--------------------------------------------------\n### PRINTF ###"
 time priTest
 
-echo -e "\n### /dev/stdout ###"
+echo -e "--------------------------------------------------\n### printf > /dev/stdout ###"
 time printfTest
 
-echo -e "\n### CAT ###"
+echo -e "--------------------------------------------------\n### CAT ###"
 time catTest
+
+echo """
+##################################################
+Verificação de varias chamadas a mesma funcao
+"""
+loopado='Sou mais rápido!'
+
+function echoLoop (){
+	for a in `seq 1024`
+	do
+					echo "$loopado" > /dev/null
+	done
+}
+
+function prinLoop (){
+	for b in `seq 1024`
+	do
+					printf "$loopado" > /dev/null
+	done
+}
+
+function prinThereLoop (){
+	for c in `seq 1024`
+	do
+					printf "$loopado" > /dev/stdout > /dev/null
+	done
+}
+function catLoop (){
+	for d in `seq 1024`
+	do
+					cat <<- EOF > /dev/null
+					$loopado
+					EOF
+	done
+}
+
+echo -e "--------------------------------------------------\n### PRINTF ###"
+time prinLoop
+
+echo -e "--------------------------------------------------\n### ECHO ###"
+time echoLoop 
+
+echo -e "--------------------------------------------------\n### printf > /dev/stdout ###"
+time prinThereLoop
+
+echo -e "--------------------------------------------------\n### CAT ###"
+time catLoop
+
